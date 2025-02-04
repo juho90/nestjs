8539b28e77b9ec622e405f6b3a4080610c4ef942
Hans <hans.han@trala.me>
2025-02-04 17:31:55 +0900

    전역에서 jwt를 사용할 수 있도록 JwtModule을 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' }, // 토큰 유효기간 1시간
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
