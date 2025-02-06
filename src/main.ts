ed90090a86ccddec5442e2837c8920a830ee2605
Hans <hans.han@trala.me>
2025-02-06 10:30:32 +0900

    프로젝트 리셋

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT ?? 3000;
-  console.log(`listen to ${port}`);
-  await app.listen(port);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
